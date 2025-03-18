a812fbb6d2bce86a483eae0694eccf55e1418838
omarmeotaz-work <omarmoetaz1997@gmail.com>
2025-03-18 14:30:51 +0200

    range and reset button update

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,54 @@ function App() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
 
-  function handlePrevStep() {
-    setStep((s) => s - 1);
-  }
-  function handleNextStep() {
-    setStep((s) => s + 1);
-  }
   function handlePrevCount() {
-    setCount((c) => c - step);
+    setCount((c) => c - Number(step));
   }
   function handleNextCount() {
-    setCount((c) => c + step);
+    setCount((c) => c + Number(step));
   }
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   const date = new Date();
   date.setDate(date.getDate() + count);
 
   return (
     <div className="conatiner">
       <div className="steps">
-        <button className="prevStep" onClick={handlePrevStep}>
-          -
-        </button>
+        <input
+          type="range"
+          min={1}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(e.target.value)}
+        />
         <p>step: {step}</p>
-        <button className="nextStep" onClick={handleNextStep}>
-          +
-        </button>
       </div>
       <div className="count">
         <button className="prevCount" onClick={handlePrevCount}>
           -
         </button>
-        <p>count: {count} </p>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        ></input>
         <button className="nextCount" onClick={handleNextCount}>
           +
         </button>
       </div>
-      {count === 0 && <span>today is </span>}
-      {count > 0 && <span>{count} days until </span>}
-      {count < 0 && <span>{-count} days ago was </span>}
-      <span>{date.toDateString()}</span>
+      <div>
+        {count === 0 && <span>today is </span>}
+        {count > 0 && <span>{count} days until </span>}
+        {count < 0 && <span>{-count} days ago was </span>}
+        <span>{date.toDateString()}</span>
+      </div>
+      {count !== 0 || step !== 1 ? (
+        <button onClick={handleReset}>Reset</button>
+      ) : null}
     </div>
   );
 }
